60476e1a115991f2861538e1bf46b7e99087c5ef
mtqiaohub <mtqiao@wustl.edu>
2025-05-22 11:14:48 -0500

    added favicon and attributions to design layout inspiration as well as icon source

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,14 @@
-import { Box, Container, Typography, Link, Button } from "@mui/material";
+import { Box, Container, Typography, Link, Button, Divider } from "@mui/material";
 import GitHub from '@mui/icons-material/GitHub';
 import Article from '@mui/icons-material/Article';
 import Email from '@mui/icons-material/Email';
 function Footer() {
     return (
         <div>
-            <Container maxWidth="lg" sx={{ py: 4 }}>
+            <Container maxWidth="lg" disableGutters sx={{ py: 4 }}>
                 <Box sx={{ textAlign: 'left' }}>
                     <Typography variant="body1" sx={{ mb: 2 }}>
-                        Suggestions or issues about the webtool? Please contact Mark Qiao {' '}
+                        Have Suggestions about or found issues with the webtool? Please contact Mark Qiao {' '}
                         <Link
                             href="mailto:mqiao@luc.edu">
                             <Email sx={{ verticalAlign: 'middle' }}></Email>
@@ -21,6 +21,24 @@ function Footer() {
                             href="mailto:jkirk2@luc.edu">
                             <Email sx={{ verticalAlign: 'middle' }}></Email>
                         </Link>
+                    </Typography>
+
+
+                    <Divider
+                        sx={{
+                            width: '100%',
+                            height: '1px',
+                            backgroundColor: '#D3D3D3',
+                            margin: '0 auto',
+                            mt: 2,
+                            mb: 2,
+                            opacity: 0.5,
+                        }}
+                    />
+                    <Typography variant="body1" sx={{ mb: 2 }}> 
+                            This project uses an icon designed by almaCELL, licensed under CC BY 4.0. No changes were made to the icon, and no endorsement is implied. <br />
+                            Webtool Layout inspired by <Link href="https://writing-assistant.github.io/#annotated-papers">A Design Space for Intelligent and Interactive Writing Assistants</Link>.
+
                     </Typography>
 
                     <Box
